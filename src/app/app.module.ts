a694505dc973cf737000bc7452a5fba4683270ee
nagi125 <nagi125.dev@gmail.com>
2021-11-23 23:43:43 +0900

    fix: ngModelサンプル追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
