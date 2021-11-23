951c3adc808e00a50c7918a5a29c7dcf127e1cab
nagi125 <nagi125.dev@gmail.com>
2021-11-23 23:39:56 +0900

    fix: ngModelを使えるようにmoduleをimport

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MembersComponent } from './members/members.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { MembersComponent } from './members/members.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
