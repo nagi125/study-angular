d22294d5958002fc3be199b4fc4c810ef6883f8e
nagi125 <nagi125.dev@gmail.com>
2021-12-01 20:23:17 +0900

    add: メンバー配列とfor文サンプル追加

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Member } from '../member';
+import { MEMBERS} from "../mock-members";
 
 @Component({
   selector: 'app-members',
@@ -9,6 +10,7 @@ import { Member } from '../member';
 
 export class MembersComponent implements OnInit {
 
+  members = MEMBERS;
   member: Member = {
     id: 1,
     name: '田中 太郎'
