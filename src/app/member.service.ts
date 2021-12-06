d76420b14145b20d0abfa8345ac7ac13c381ced0
nagi125 <nagi125.dev@gmail.com>
2021-12-06 23:10:20 +0900

    fix: Httpクライアント処理の中間処理・エラーハンドリングを追加

--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -3,21 +3,44 @@ import { Member } from './member';
 import { MEMBERS } from './mock-members';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient } from "@angular/common/http";
+import { catchError, map , tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  private membersUrl = 'api/members';
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+      private http: HttpClient,
+      private messageService: MessageService
+  ) { }
 
   getMembers(): Observable<Member[]> {
-    this.messageService.add('MemberService: 社員一覧データを取得しました');
-    return of(MEMBERS);
+    return this.http.get<Member[]>(this.membersUrl)
+    .pipe(
+        tap(members => this.log('社員データを取得しました')),
+        catchError(this.handleError<Member[]>('getMembers', []))
+    );
   }
 
   getMember(id: number): Observable<Member | undefined> {
     this.messageService.add(`MemberService: 社員データ(id=${id})を取得しました`);
     return of(MEMBERS.find((member: Member) => member.id === id));
+  }
+
+  private log(message: string) {
+    this.messageService.add(`MemberService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+
+      this.log(`${operation} 失敗: ${error.message}`)
+
+      return of(result as T);
+    }
   }
 }
